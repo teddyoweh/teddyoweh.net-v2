b00e450529fab5b6945200b9193b1d055c2dbd8f
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-05-08 13:21:46 -0500

    fix lint errors [fonts]

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,6 +5,7 @@ import './styles/home.scss'
 import './styles/about.scss'
 import './styles/research.scss'
 import './styles/projects.scss'
+import teddyimg from './assets/teddy.jpeg'
 import logo from './assets/TO.png'
 const inter = Inter({ subsets: ['latin'] })
 import Head from 'next/head';
@@ -31,12 +32,14 @@ export default function RootLayout({
       <meta property="og:title" content="Teddy Oweh - Software Engineer (ML)" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://teddyoweh.net" />
-    <meta property="og:image" content="./assets/teddy.jpeg" />
+    <meta property="og:image" content={teddyimg.src} />
     <meta property="og:description" 
   content="Software Engineer (ML), CS (Artificial Intelligence & Data Science) + Math." />
   <meta property="og:site_name" content="TeddyOweh" />
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'/>
-
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" />
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Nanum+Pen+Script&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
       </head>
     
       <body className={inter.className}>{children}</body>
