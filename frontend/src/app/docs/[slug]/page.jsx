df5e951300b213a41e1c7770fe9e94f0b4eab8b2
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-05-09 10:11:07 -0500

    optimized build + docs page

--- frontend/src/app/docs/[slug]/page.jsx
+++ frontend/src/app/docs/[slug]/page.jsx
@@ -1,16 +1,16 @@
 'use client'
-import teddyimg from '../assets/teddy.jpeg'
-import NavigationBar from '../components/NavigationBar' 
-import Footer from '../components/Footer'
+import teddyimg from '../../assets/teddy.jpeg'
+import NavigationBar from '../../components/NavigationBar' 
+import Footer from '../../components/Footer'
 import Image from 'next/image'
 import Link from 'next/link'
-import researchteam from '../assets/researchteam.jpeg'
-import diseaseres from '../assets/disease1.jpeg'
-import hfsres from '../assets/hfs.jpeg'
-import nasares from '../assets/nasares.jpeg'
+import researchteam from '../../assets/researchteam.jpeg'
+import diseaseres from '../../assets/disease1.jpeg'
+import hfsres from '../../assets/hfs.jpeg'
+import nasares from '../../assets/nasares.jpeg'
 import { usePathname,useRouter } from 'next/navigation';
-import badgeres from '../assets/badge.jpeg'
-import {docs} from './page-data'
+import badgeres from '../../assets/badge.jpeg'
+import {docs} from '../page-data'
 export default function DocsPage({params}){
     const router = useRouter();
 
