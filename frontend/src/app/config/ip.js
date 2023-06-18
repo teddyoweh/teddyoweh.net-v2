cd0ad55b84304be2bbe94bd7a9b465c9170b58ec
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-06-18 15:22:28 -0700

    Update ip.js

--- frontend/src/app/config/ip.js
+++ frontend/src/app/config/ip.js
@@ -1,5 +1,5 @@
 "use client"
 // const serverip = 'http://teddyoweh-backend.up.railway.app'
-const serverip = 'https://teddyoweh-net-backend-ed5a0e3eae91.herokuapp.com/'
+const serverip = 'https://teddyoweh-net-backend-ed5a0e3eae91.herokuapp.com'
 // i see you :)
 export {serverip}
