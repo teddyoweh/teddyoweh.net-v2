a995e026d2bf7d342e51339c01b55d91162d54e3
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-06-18 20:47:36 -0700

    Update page-data.js

--- frontend/src/app/docs/page-data.js
+++ frontend/src/app/docs/page-data.js
@@ -1,4 +1,5 @@
 'use client'
+ 
 const docs = [
 {
     'title':'Probabilistic Methods for State Estimation',
@@ -6,7 +7,8 @@ const docs = [
 },
 {
     'title':'The Intersection of Generative Language Modeling, Generative Adversarial Networks, and OneShot Learning: Implications for Psychological Research and Biopsychology',
-    'desc':'The integration of generative language modeling, GANs, and one-shot learning techniques into psychological research has the potential to revolutionize the field by providing new avenues for studying language processing, neural correlates of cognition and behavior, memory encoding and retrieval, and cognitive development, ultimately leading to more accurate diagnostics, effective treatments, and targeted interventions for individuals with communication disorders, neurological diseases, and developmental disorders.'
+    'desc':'The integration of generative language modeling, GANs, and one-shot learning techniques into psychological research has the potential to revolutionize the field by providing new avenues for studying language processing, neural correlates of cognition and behavior, memory encoding and retrieval, and cognitive development, ultimately leading to more accurate diagnostics, effective treatments, and targeted interventions for individuals with communication disorders, neurological diseases, and developmental disorders.',
+    'src':'./files/The Intersection of Generative Language Modeling.pdf'
 }
 ]
 
